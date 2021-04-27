5da7b26b4ec889031ba272294462c59db9c12d51
kshitijD38 <47937542+kshitijD38@users.noreply.github.com>
2021-04-27 21:18:29 +0530

    wswsws

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./styles.css";
 
 export default function App() {
   const [user, setUser] = useState([]);
-  const [user1, setUser2] = useState([]);
 
   const api = "https://randomuser.me/api";
 
@@ -15,44 +14,22 @@ export default function App() {
     setUser([...user, dataJson.results[0]]);
   };
 
-  const ascArray = [...user];
-  ascArray.sort((a, b) => {
-    if (a.name.first > b.name.first) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
-
-  // console.log("user is >>>>>", user);
+  console.log("user is >>>>>", user);
 
   return (
     <div className="App">
       <button onClick={addUserHandler}>Add User</button>
-      <button>Asc</button>
-      <div>
-        {user ? (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              border: 1
-            }}
-          >
-            {user?.map((i, index) => (
-              <div key={index}>
-                <span>
-                  <div>
-                    {i.name.title} {i.name.first} {i.name.last} {i.gender}{" "}
-                    {i.email}{" "}
-                  </div>
-                </span>
-              </div>
-            ))}
+      <h3>
+        {user?.map((u, index) => (
+          <div key={index}>
+            {u.name.title}
+            {u.name.first}
+            {u.name.last} <br />
+            {u.email} <br />
+            {u.gender}
           </div>
-        ) : null}
-      </div>
+        ))}
+      </h3>
     </div>
   );
 }
